a5fa5369d459098fd3aea3b49558b8ba54ef4f15
anber137 <87627609+anber137@users.noreply.github.com>
2022-07-19 14:14:23 +0700

    Update mian.tf

--- mian.tf
+++ mian.tf
@@ -1,19 +1,2 @@
-terraform {
-  required_version = ">= 0.12"
-}
+resource "null_resource" "example" {}
 
-provider "aws" {
-  region = "us-west-2"
-}
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners = ["amazon"]
-  filter {
-    name =  "name"
-    values = ["amzn-ami-hvm-*-x86_64-gp2"]
-  }
-  filter {
-    name =  "owner-alias"
-    values = ["amazon"]
-  }
-}
